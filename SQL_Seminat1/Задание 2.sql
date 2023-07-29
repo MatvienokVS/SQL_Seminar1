5a9f38b82536963b79c35549b040d816ac7ebab2
MatvienokVS <skavaava@gmail.com>
2023-07-29 22:03:08 +0300

    ДЗ Семинар 1

---
+++ SQL_Seminat1/Задание 2.sql
@@ -0,0 +1,4 @@
+USE sql_seminar1;
+SElect tel.Колличество 
+from tel
+where tel.Колличество > 2;
---
+++ SQL_Seminat1/Задание 3.sql
@@ -0,0 +1,4 @@
+USE sql_seminar1;
+Select Производитель, Марка
+FROM sql_seminar1.tel
+where tel.Производитель = 'Huawei';
